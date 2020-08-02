293c9a0807f5d4bebea24c938e9dacbf96ea9d4a
Vahagn <vhgn@pm.me>
2020-08-03 01:47:07 +0400

    Add Environments enum

---
+++ src/environments.ts
@@ -0,0 +1,9 @@
+export enum Environment {
+	Development = 0,
+	Production = 1,
+}
+
+export const endpoints = [
+	'https://ipaytest.arca.am:8445/payment/rest',
+	'https://ipay.arca.am/payment/rest',
+];
